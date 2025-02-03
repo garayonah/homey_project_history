06a7cecc01cb122dd2a7f2e7dc64a8dfccbfeba7
garayonah <garayonah@gmail.com>
2025-02-04 00:54:16 +0200

    Added a sample conversation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-User.create! email_address: "yg@email.com", password: "abc123", password_confirmation: "abc123"
-User.create! email_address: "person_2@email.com", password: "abc123", password_confirmation: "abc123"
-User.create! email_address: "colleague@email.com", password: "abc123", password_confirmation: "abc123"
-Project.create!(name: "Homey Task", status: "open")
+
+#Create Users
+user1 = User.create! username: "yonah", email_address: "yg@email.com", password: "abc123", password_confirmation: "abc123"
+user2 = User.create! username: "person", email_address: "person_2@email.com", password: "abc123", password_confirmation: "abc123"
+user3 = User.create! username: "colleague", email_address: "colleague@email.com", password: "abc123", password_confirmation: "abc123"
+
+#Create Project
+project = Project.create!(name: "Project History Sample Discussion", status: "open")
+
+#Create Conversation Example
+ProjectHistory.create!(project: project, user: user1, status_before: 'initial', status_after: 'initial', comment: 'Is this going to be used for one project or multiple?')
+
+ProjectHistory.create!(project: project, user: user2, status_before: 'initial', status_after: 'open', comment: 'Lets make it possible for multiple.')
+project.update(status: 'open')
+
+ProjectHistory.create!(project: project, user: user1, status_before: 'open', status_after: 'open', comment: 'What statuses should be allowed?')
+
+ProjectHistory.create!(project: project, user: user3, status_before: 'open', status_after: 'open', comment: 'Until a definitive list is chosen, leave it open to the user')
+
+ProjectHistory.create!(project: project, user: user1, status_before: 'open', status_after: 'pending_approval', comment: 'Anything else pressing for now?')
+project.update(status: 'pending_approval')
+
+ProjectHistory.create!(project: project, user: user3, status_before: 'open', status_after: 'closed', comment: 'We shall close the project now until further notice')
+project.update(status: 'closed')
